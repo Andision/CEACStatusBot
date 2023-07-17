4c6c50ffb304adcb0fe6f7fe7707026eb527988a
Andision <48558940+Andision@users.noreply.github.com>
2023-07-17 18:11:57 +0800

    Feature: Add multiple receive email

--- CEACStatusBot/notification/email.py
+++ CEACStatusBot/notification/email.py
@@ -9,14 +9,14 @@ class EmailNotificationHandle(NotificationHandle):
     def __init__(self,fromEmail:str,toEmail:str,emailPassword:str,hostAddress:str) -> None:
         super().__init__()
         self.__fromEmail = fromEmail
-        self.__toEmail = toEmail
+        self.__toEmail = toEmail.split("|")
         self.__emailPassword = emailPassword
         self.__hostAddress = hostAddress
 
     def __init__(self,fromEmail:str,toEmail:str,emailPassword:str) -> None:
         super().__init__()
         self.__fromEmail = fromEmail
-        self.__toEmail = toEmail
+        self.__toEmail = toEmail.split("|")
         self.__emailPassword = emailPassword
         self.__hostAddress = "smtp."+fromEmail.split("@")[1]
         
@@ -25,17 +25,16 @@ class EmailNotificationHandle(NotificationHandle):
         
         # {'success': True, 'visa_type': 'NONIMMIGRANT VISA APPLICATION', 'status': 'Issued', 'case_created': '30-Aug-2022', 'case_last_updated': '19-Oct-2022', 'description': 'Your visa is in final processing. If you have not received it in more than 10 working days, please see the webpage for contact information of the embassy or consulate where you submitted your application.', 'application_num': '***'}
 
-        receiver = [self.__toEmail]
         mail_title = '[CEACStatusBot] {} : {}'.format(result["application_num_origin"],result['status'])
         mail_content = str(result)
 
         msg = MIMEMultipart()
         msg["Subject"] = Header(mail_title,'utf-8')
         msg["From"] = self.__fromEmail
-        msg['To'] = ";".join(receiver)
+        msg['To'] = ";".join(self.__toEmail)
         msg.attach(MIMEText(mail_content,'plain','utf-8'))
 
         smtp = SMTP_SSL(self.__hostAddress) # ssl登录
         print(smtp.login(self.__fromEmail,self.__emailPassword))
-        print(smtp.sendmail(self.__fromEmail,receiver,msg.as_string()))
+        print(smtp.sendmail(self.__fromEmail,self.__toEmail,msg.as_string()))
         smtp.quit()
